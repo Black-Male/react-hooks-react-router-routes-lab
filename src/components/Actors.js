3d9c5b1a44ce2529ec852ab72e5d27370990f6a8
Black-Male <Gkaranja20@gmail.com>
2023-01-21 12:38:19 +0300

    Work Done

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,26 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((z,index)=>{
+        return(
+          <div key={index}>
+            <h2>Name:{z.name}</h2>
+            <p>Movies:</p>
+            <ul>
+              {z.movies.map((y,newIndex)=>{
+                return(
+                  <li key={newIndex}>{y}</li>
+                )
+              })}
+            </ul>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,28 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+    <h1>Movies Page</h1>
+    {movies.map((z,index)=>{
+      return(
+        <div key={index}>
+          <h2>Title:{z.title}</h2>
+          <p>Time:{z.time}</p>
+          <p>Genres:</p>
+          <ul>
+            {z.genres.map((y,newIndex)=>{
+              return(
+                <li key={newIndex}>{y}</li>
+              )
+            })}
+          </ul>
+        </div>
+      )
+    })}
+  </div>
+  )
+  ;
 }
 
 export default Movies;
